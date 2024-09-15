8d74608e06201ed6e99626ad828960ac650d6512
larryfitz5 <lirianoa2454@student.faytechcc.edu>
2024-09-15 19:36:51 -0400

    Add files via upload

---
+++ p1lab1 lirianocolon.py
@@ -0,0 +1,13 @@
+# Your Name
+# Date
+# Introduction to Python 3
+# This program welcomes the user by their first and last name.
+
+# Get the user's first name
+first_name = input("Please enter your first name: ")
+
+# Get the user's last name
+last_name = input("Please enter your last name: ")
+
+# Display a welcome message
+print(f"Welcome, {first_name} {last_name}, to this course!")
